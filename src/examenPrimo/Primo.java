71e6a6f2833aae13521fc908433514ceaeccea49
eless <eless@PortElessar>
2025-05-21 11:50:04 +0200

    asi es la rama 1

--- src/examenPrimo/Primo.java
+++ src/examenPrimo/Primo.java
@@ -11,7 +11,11 @@ public class Primo {
 		System.out.println("Buenos dias, soy Elessar");
 		System.out.println("Introduce un numero: ");
 		int numero = ent.nextInt();
-		
+		if(numero == 3 || numero == 7 || numero ==11) {
+			System.out.println(numero + " es primo");
+		}else {
+			System.out.println("no es primo");
+		}
 	}
 
 }
