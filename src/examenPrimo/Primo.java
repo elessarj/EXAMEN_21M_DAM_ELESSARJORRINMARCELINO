27868b2e85fe027022c19ff5e2892c2bf624d336
eless <eless@PortElessar>
2025-05-21 11:30:17 +0200

    programa base

---
+++ src/examenPrimo/Primo.java
@@ -0,0 +1,17 @@
+package examenPrimo;
+
+import java.util.Scanner;
+
+public class Primo {
+
+	public static void main(String[] args) {
+
+		Scanner ent = new Scanner(System.in);
+		
+		System.out.println("Buenos dias, soy Elessar");
+		System.out.println("Introduce un numero: ");
+		int numero = ent.nextInt();
+		
+	}
+
+}
